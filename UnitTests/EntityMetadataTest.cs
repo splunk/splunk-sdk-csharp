f3b1615cdaaa64112f4f0d2380bbe49c37582687
Wim Colgate <wcolgate!@splunk.com>
2012-09-10 11:30:49 -0700

    Add missing metadat test file (TBD body)
    Add .suo for VS2012.

---
+++ UnitTests/EntityMetadataTest.cs
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012 Splunk, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Splunk;
+
+    /// <summary>
+    /// Test Metadata types.
+    /// </summary>
+    [TestClass]
+    public class MetadataTypeTest : TestHelper
+    {
+        /// <summary>
+        /// Assert string root
+        /// </summary>
+        private static string assertRoot = "Metadata Type assert: ";
+
+        /// <summary>
+        /// Test the Metadata types.
+        /// </summary>
+        [TestMethod]
+        public void MetadataType()
+        {
+            // TBD
+        }
+
+    }
+}
+
+
