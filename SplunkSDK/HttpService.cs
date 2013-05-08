af99da396bcbab3ad63c005523209a57196452d9
(Andy) Yunxin Wu <ywu@splunk.com>
2013-05-08 11:12:54 -0700

    Removed unnecessary try catch in HttpService.Send

--- SplunkSDK/HttpService.cs
+++ SplunkSDK/HttpService.cs
@@ -397,19 +397,12 @@ namespace Splunk
                 // Get the bytes for proper encoded length when going over the 
                 // wire.
                 byte[] bytes = Encoding.UTF8.GetBytes((string)content);
-                try
+                webRequest.ContentLength = bytes.GetLength(0);
+                using (var stream = webRequest.GetRequestStream())
+                using (var streamWriter = new StreamWriter(stream))
                 {
-                    webRequest.ContentLength = bytes.GetLength(0);
-                    Stream stream = webRequest.GetRequestStream();
-                    StreamWriter streamWriter = new StreamWriter(stream);
                     streamWriter.Write(content);
                     streamWriter.Flush();
-                    stream.Close();
-                }
-                catch (Exception e)
-                {
-                    System.Console.WriteLine("Exception: " + e.Message);
-                    throw;
                 }
             }
 
