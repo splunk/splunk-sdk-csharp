e4fdb1fa2680f5eae389880f154ab877a0d38787
(Andy) Yunxin Wu <ywu@splunk.com>
2013-05-15 13:55:06 -0700

    CheckPoint

---
+++ examples/sharepoint_web_part/IndexSummaryWebPart/IndexSummaryWebPart.ascx
@@ -0,0 +1,17 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %> 
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %> 
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="IndexSummaryWebPart.ascx.cs" Inherits="Splunk.Examples.SharePointWebPart.IndexSummaryWebPart.IndexSummaryWebPart" %>
+<asp:GridView ID="IndexSummaryGridView" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" EnableModelValidation="True" GridLines="Vertical">
+    <AlternatingRowStyle BackColor="#DCDCDC" />
+    <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+    <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+    <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+    <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+</asp:GridView>
+
---
+++ examples/sharepoint_web_part/IndexSummaryWebPart/IndexSummaryWebPart.ascx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using SplunkSDKHelper;
+
+namespace Splunk.Examples.SharePointWebPart.IndexSummaryWebPart
+{
+    [ToolboxItemAttribute(false)]
+    public partial class IndexSummaryWebPart : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public IndexSummaryWebPart()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get info on how 
+            var cli = Command.Splunk();
+
+            cli.AddRule("search", typeof(string), "search string");
+            if (!cli.Opts.ContainsKey("search"))
+            {
+                System.Console.WriteLine(
+                    "Search query string required, use --search=\"query\"");
+                Environment.Exit(1);
+            }
+
+            var service = Service.Connect(cli.Opts);
+
+            var outArgs = new JobResultsArgs
+            {
+                OutputMode = JobResultsArgs.OutputModeEnum.Xml,
+
+                // Return all entries.
+                Count = 0,
+            };
+
+            using (var stream = service.Oneshot(
+                (string)cli.Opts["search"], outArgs))
+            {
+                using (var rr = new ResultsReaderXml(stream))
+                {
+                    foreach (var @event in rr)
+                    {
+                        System.Console.WriteLine("EVENT:");
+                        foreach (string key in @event.Keys)
+                        {
+                            System.Console.WriteLine(
+                                "   " + key + " -> " + @event[key]);
+                        }
+                    }
+                }
+            }
+            this.IndexSummaryGridView.DataSource
+               = new List<IndexSummaryItem>
+                    {
+                        new IndexSummaryItem
+                            {
+                                SourceType = "test",
+                            }
+                    };
+            this.IndexSummaryGridView.DataBind();
+        }
+    }
+}
